64cc961e34e6779486a324bc01b89d23e2b82858
Anthony Suárez <neolight1010@gmail.com>
2021-07-07 21:39:40 -0500

    Setup three.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
-import './style.css'
+import "./style.css";
+import * as three from "three";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const scene = new three.Scene();
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const camera = new three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new three.WebGLRenderer({
+  canvas: document.querySelector("#bg") as HTMLCanvasElement,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+// Add geometry
+const geometry = new three.TorusGeometry(10, 3, 16, 100);
+const material = new three.MeshBasicMaterial({
+  color: 0xff7437,
+  wireframe: true,
+});
+const torus = new three.Mesh(geometry, material);
+
+scene.add(torus);
+
+// Animate
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+
+  torus.rotateX(0.02);
+}
+
+animate();
