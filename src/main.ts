9ad6e8c6781f926613d3d73965a0ae548af34b3b
Anthony Suárez <neolight1010@gmail.com>
2021-07-09 16:14:13 -0500

    Add async main function.

--- src/main.ts
+++ src/main.ts
@@ -6,71 +6,75 @@ import { loadStethoscopeObject } from "./objects/stethoscope";
 import { Clock } from "three";
 import ellipseMotion from "./motion_funcs/ellipse";
 
-// Scene setup.
-const scene = new three.Scene();
+export async function main(): Promise<void> {
+  // Scene setup.
+  const scene = new three.Scene();
 
-const camera = new three.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.setZ(150);
+  const camera = new three.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.setZ(150);
 
-const renderer = new three.WebGLRenderer({
-  canvas: document.querySelector("#bg") as HTMLCanvasElement,
-});
+  const renderer = new three.WebGLRenderer({
+    canvas: document.querySelector("#bg") as HTMLCanvasElement,
+  });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-const clock = new Clock(true);
+  const clock = new Clock(true);
 
-// Add objects
-const textureLoader = new three.TextureLoader();
-const background = textureLoader.load("assets/background/neon.png");
-scene.background = background;
+  // Add objects
+  const textureLoader = new three.TextureLoader();
+  const background = textureLoader.load("assets/background/neon.png");
+  scene.background = background;
 
-const [light1, lightHelper1] = pointLightWithHelper(0, 30, 0, 0xf533e8);
-scene.add(light1);
-scene.add(lightHelper1);
+  const [light1, lightHelper1] = pointLightWithHelper(0, 30, 0, 0xf533e8);
+  scene.add(light1);
+  scene.add(lightHelper1);
 
-const [light2, lightHelper2] = pointLightWithHelper(40, 30, 30, 0x89f51d);
-scene.add(light2);
-scene.add(lightHelper2);
+  const [light2, lightHelper2] = pointLightWithHelper(40, 30, 30, 0x89f51d);
+  scene.add(light2);
+  scene.add(lightHelper2);
 
-const [light3, lightHelper3] = pointLightWithHelper(-40, 30, -30, 0xf4ede8);
-scene.add(light3);
-scene.add(lightHelper3);
+  const [light3, lightHelper3] = pointLightWithHelper(-40, 30, -30, 0xf4ede8);
+  scene.add(light3);
+  scene.add(lightHelper3);
 
-const [light4, lightHelper4] = pointLightWithHelper(0, -40, 30);
-scene.add(light4);
-scene.add(lightHelper4);
+  const [light4, lightHelper4] = pointLightWithHelper(0, -40, 30);
+  scene.add(light4);
+  scene.add(lightHelper4);
 
-const laptop = await load_laptop_object();
-laptop.applyMatrix4(new three.Matrix4().makeScale(0.8, 0.8, 0.8)); // Scale 0.8x
-laptop.translateY(-10);
-scene.add(laptop);
+  const laptop = await load_laptop_object();
+  laptop.applyMatrix4(new three.Matrix4().makeScale(0.8, 0.8, 0.8)); // Scale 0.8x
+  laptop.translateY(-10);
+  scene.add(laptop);
 
-const stethoscope = await loadStethoscopeObject();
-scene.add(stethoscope);
+  const stethoscope = await loadStethoscopeObject();
+  scene.add(stethoscope);
 
-// Animate
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  animate();
 
-  laptop.rotateY(Math.PI / 150);
-  laptop.rotateZ(Math.PI / 400);
+  // Animate
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 
-  // stethoscope orbit motion
-  [
-    stethoscope.position.x,
-    stethoscope.position.z,
-    stethoscope.position.y,
-  ] = ellipseMotion(clock.getElapsedTime(), 110, 80, 0, -15);
+    laptop.rotateY(Math.PI / 150);
+    laptop.rotateZ(Math.PI / 400);
 
-  stethoscope.rotateY(Math.PI / -200);
+    // stethoscope orbit motion
+    [
+      stethoscope.position.x,
+      stethoscope.position.z,
+      stethoscope.position.y,
+    ] = ellipseMotion(clock.getElapsedTime(), 110, 80, 0, -15);
+
+    stethoscope.rotateY(Math.PI / -200);
+  }
 }
 
-animate();
+main();
