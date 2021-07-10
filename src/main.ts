44c1c22513c5f3243c0d298873f0c6858fdd7a06
Anthony Suárez <neolight1010@gmail.com>
2021-07-10 11:59:31 -0500

    Add responsiveness.

--- src/main.ts
+++ src/main.ts
@@ -37,13 +37,9 @@ export async function main(): Promise<void> {
   // Scene setup.
   const scene = new three.Scene();
 
-  const camera = new three.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.setZ(150);
+  const camera = new three.PerspectiveCamera(75, 2, 0.1, 1000);
+  camera.position.setZ((1536 / window.innerWidth) * 120);
+  camera.updateProjectionMatrix();
 
   const renderer = new three.WebGLRenderer({
     canvas: document.querySelector("#bg") as HTMLCanvasElement,
@@ -109,6 +105,10 @@ export async function main(): Promise<void> {
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
 
     laptop.rotateY(Math.PI / 150);
     laptop.rotateZ(Math.PI / 400);
