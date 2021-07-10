5866cf21afae6f1c4f1b8905296ae8b842f65c5e
Anthony Suárez <neolight1010@gmail.com>
2021-07-10 13:57:24 -0500

    Set up perform page.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/proyecto-arte/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        perform: resolve(__dirname, "perform.html"),
+      },
+    },
+  },
 });
