0e645c586a37a98d56a773a0e1ac0e21023c147d
Anthony Suárez <neolight1010@gmail.com>
2021-07-10 22:12:52 -0500

    Set up gallery.html

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         perform: resolve(__dirname, "perform.html"),
+        gallery: resolve(__dirname, "gallery.html"),
       },
     },
   },
