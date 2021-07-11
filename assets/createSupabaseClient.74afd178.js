dd14fff453dca11152618ab165969b7d01f72f33
NeoLight1010 <58057324+NeoLight1010@users.noreply.github.com>
2021-07-11 04:57:33 +0000

    Deploying to gh-pages from @ NeoLight1010/proyecto-arte@73af1f2ad0eaeb807f00cb8872a065240c2b7180 🚀

---
+++ assets/createSupabaseClient.74afd178.js
@@ -0,0 +1,1 @@
+import{j as c}from"./vendor.dfa3fcc4.js";function o(){return c("https://cgehbsesbyvufdvehrgv.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTk2ODc0MiwiZXhwIjoxOTQxNTQ0NzQyfQ.qkg7c6tnc0w5z-h-1XToCrChqXLbgoKv01R2GbOojaU")}export{o as c};
